5e23879d0e8f85d451b1aa10defe28230d6d4d8b
mir-aclewhip <mna271@nyu.edu>
2018-01-28 14:06:34 -0500

    initializing project

---
+++ bot.js
@@ -0,0 +1,5 @@
+const binance = require('node-binance-api');
+binance.options({
+  'APIKEY':'AZNp6h9Kaoeukj8b9uZgwVZuRf4n0AXLjrwA9gKbg0JGog4ECrk5yaCNmFsldnNQ',
+  'APISECRET':'uQp8M62dDDylqBvAwvqfNVIgsScOZEXnp4lnxZ0wF2WoamQt2eLQgvMfbVEIchhc'
+});
