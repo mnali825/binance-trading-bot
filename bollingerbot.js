76a6834b85af5e0414f61e2c8686f2f75a02be85
Ali <mali@rfbd.org>
2018-02-16 11:02:52 -0500

    removed keys

--- bollingerbot.js
+++ bollingerbot.js
@@ -1,7 +1,7 @@
 const binance = require('node-binance-api');
 binance.options({
-  'APIKEY':'AZNp6h9Kaoeukj8b9uZgwVZuRf4n0AXLjrwA9gKbg0JGog4ECrk5yaCNmFsldnNQ',
-  'APISECRET':'uQp8M62dDDylqBvAwvqfNVIgsScOZEXnp4lnxZ0wF2WoamQt2eLQgvMfbVEIchhc'
+  'APIKEY':'',
+  'APISECRET':''
 });
 
 var BB = require('technicalindicators').BollingerBands
